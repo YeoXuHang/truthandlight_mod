d4cc56b505245033a61789168c04941858611898
masteroffinal <101001741+moffinal@users.noreply.github.com>
2022-03-07 20:50:23 +0800

    Update TruthAndLightModCreativeTab.java

--- src/main/java/com/mos/truthandlight/item/TruthAndLightModCreativeTab.java
+++ src/main/java/com/mos/truthandlight/item/TruthAndLightModCreativeTab.java
@@ -7,7 +7,19 @@ public class TruthAndLightModCreativeTab {
     public static final CreativeModeTab TruthAndLightModCreativeTab = new CreativeModeTab("truthandlight_tab") {
         @Override
         public ItemStack makeIcon() {
+            return new ItemStack(TruthAndLightModItem.CHROMIUM.get());
+        }
+    };
+         public ItemStack makeIcon() {
             return new ItemStack(TruthAndLightModItem.RAW_CHROMIUM.get());
         }
     };
-}
+     public ItemStack makeIcon() {
+            return new ItemStack(TruthAndLightModItem.CHROMIUM_SWORD.get());
+        }
+    };
+     public ItemStack makeIcon() {
+            return new ItemStack(TruthAndLightModItem.CHROMIUM_HELMET.get());
+        }
+    };
+
