1b98324bd013a66c12bdb5502ed8e68e81aa4034
masteroffinal <101001741+moffinal@users.noreply.github.com>
2022-03-09 20:10:19 +0800

    Just added sky light items into the mod
    Just an Brand New Set

--- src/main/java/com/mos/truthandlight/item/TruthAndLightModItem.java
+++ src/main/java/com/mos/truthandlight/item/TruthAndLightModItem.java
@@ -66,9 +66,46 @@ public class TruthAndLightModItem {
   public static final RegistryObject<Item> CHROMIUM_BOOTS = ITEMS.register("chromium_boots",
           () -> new ArmorItem(ModArmorMaterials.CHROMIUM, EquipmentSlot.FEET,
                   new Item.Properties().tab(ModCreativeModeTab.ARMOR)));
+  
+  public static final RegistryObject<Item> SKY_LIGHT_SWORD = ITEMS.register("sky_light_sword" ,
+          () -> new SwordItem(ModTiers.SKY_LIGHT,9, 0.5f,
+                  new Item.Properties().tab(ModCreativeModeTab.WEAPONSANDTOOLS)));
+
+  public static final RegistryObject<Item> SKY_LIGHT_PICKAXE = ITEMS.register("sky_light_pickaxe" ,
+          () -> new PickaxeItem(ModTiers.SKY_LIGHT,2, 0.5f,
+                  new Item.Properties().tab(ModCreativeModeTab.WEAPONSANDTOOLS)));
+
+  public static final RegistryObject<Item> SKY_LIGHT_SHOVEL = ITEMS.register("sky_light_shovel" ,
+          () -> new ShovelItem(ModTiersSKY_LIGHT,2, 0.5f,
+                  new Item.Properties().tab(ModCreativeModeTab.WEAPONSANDTOOLS)));
+
+  public static final RegistryObject<Item> SKY_LIGHT_AXE = ITEMS.register("sky_light_axe" ,
+          () -> new AxeItem(ModTiers.SKY_LIGHT,10, 0.5f,
+                  new Item.Properties().tab(ModCreativeModeTab.WEAPONSANDTOOLS)));
+
+  public static final RegistryObject<Item> SKY_LIGHT_HOE = ITEMS.register("sky_light_hoe" ,
+          () -> new HoeItem(ModTiers.SKY_LIGHT,1, 0.5f,
+                  new Item.Properties().tab(ModCreativeModeTab.WEAPONSANDTOOLS)));
+  
+   public static final RegistryObject<Item> SKY_LIGHT_HELMET = ITEMS.register("sky_light_helmet",
+          () -> new ArmorItem(ModArmorMaterials.SKY_LIGHT, EquipmentSlot.HEAD,
+                  new Item.Properties().tab(ModCreativeModeTab.ARMOR)));
+
+  public static final RegistryObject<Item> SKY_LIGHT_CHESTPLATE = ITEMS.register("sky_light_chestplate",
+          () -> new ArmorItem(ModArmorMaterials.SKY_LIGHT, EquipmentSlot.CHEST,
+                  new Item.Properties().tab(ModCreativeModeTab.ARMOR)));
+
+  public static final RegistryObject<Item> SKY_LIGHT_LEGGINGS = ITEMS.register("sky_light_leggings",
+          () -> new ArmorItem(ModArmorMaterials.SKY_LIGHT, EquipmentSlot.LEGS,
+                  new Item.Properties().tab(ModCreativeModeTab.ARMOR)));
+
+  public static final RegistryObject<Item> SKY_LIGHT_BOOTS = ITEMS.register("sky_light_boots",
+          () -> new ArmorItem(ModArmorMaterials.SKY_LIGHT, EquipmentSlot.FEET,
+                  new Item.Properties().tab(ModCreativeModeTab.ARMOR)));
 
   public static final RegistryObject<Item> CHROMIUM_BOW = ITEMS.register("chromium_bow",
           () -> new BowItem(new Item.Properties().tab(ModCreativeModeTab.WEAPONSANDTOOLS).durability(2000)));
+  
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
